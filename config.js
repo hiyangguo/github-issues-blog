219311562973554ae596e57a2aaa9d586d01b2af
Godefery <hiyangguo@qq.com>
2016-04-15 23:27:06 +0800

    update

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var _config = {
-    blog_name: '用于演示的博客',         // 博客名称
+    blog_name: '兔爸的博客',         // 博客名称
     owner: 'hiyangguo',           // github 用户名
     repo: 'hiyangguo.github.io',// github 中对应项目名
     duoshuo_id: 'hiyangguo',            // 在第三方评论插件多说申请站点的子域名
-    access_token: '5384dabad242b41e58b59fbbab00d2a1f77ac18b',       //github token
-    per_page: '15'                    // 默认一页显示几篇文章
+    //access_token: '',       //github token
+    per_page: '10'                    // 默认一页显示几篇文章
 }
 
 var duoshuoQuery = {short_name: _config['duoshuo_id']};
