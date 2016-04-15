d2e752f303f607c3512fdf4b0d5a92184e569c17
Godefery <hiyangguo@qq.com>
2016-04-15 22:29:08 +0800

    change config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
     blog_name: '用于演示的博客',         // 博客名称
     owner: 'hiyangguo',           // github 用户名
-    repo: 'github-issues-blog',// github 中对应项目名
+    repo: 'hiyangguo.github.io',// github 中对应项目名
     duoshuo_id: 'hiyangguo',            // 在第三方评论插件多说申请站点的子域名
     access_token: '404cc20f899bae35d54ed75dc257959dba10e402',       //github token
     per_page: '15'                    // 默认一页显示几篇文章
